958851bf781fe133c5b768c12c2a847f2e9a4c63
Soskin Andrey <arthas@-.localdomain>
2023-06-01 21:40:06 +0700

    13

---
+++ 13.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+char *scpy(char *to, char *from) 
+{
+  for (size_t i = 0; i <= strlen(from); i++) 
+  {
+    to[i] = from[i];
+  }
+  return to;
+}
+
+int main()
+{
+    char str[] = "qwerty";
+    char str2[10];
+    scpy(str2, str);
+    for (int i = 0; i < strlen(str2); i++)
+    {
+        printf("%c ", str2[i]);
+    }
+    printf("\n%d\n", strlen(str2));
+    return 0;
+}
