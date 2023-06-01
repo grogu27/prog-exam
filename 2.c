eeafe7a33d4734e951213cce77c16f63003cb36a
None <you@example.com>
2023-06-01 23:10:19 +0700

    2

---
+++ 2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+  char symbols[] = "leon";
+  char str[] = "leonnneado dicaprio";
+  char result[sizeof(str)];
+
+for(int i = 0; i < sizeof(str); i++){
+    int correct = 0;
+    for(int j = 0; j < sizeof(symbols); j++){
+        if(str[i] == symbols[j])
+            correct = 1;
+    }
+    if(correct){
+        result[i] = str[i];
+    }
+    else{
+        result[i] = '\0';
+        break;
+    }
+  }
+  printf("%s\n",result);
+  return 0;
+}
